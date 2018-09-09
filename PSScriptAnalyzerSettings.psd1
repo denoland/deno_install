254d02b14073ab64f6e09220e531fca4d892b11c
Mark Tiedemann <www.marktiedemann@gmail.com>
2018-09-09 18:41:45 +0200

    Format script

---
+++ PSScriptAnalyzerSettings.psd1
@@ -0,0 +1,59 @@
+﻿# See: https://github.com/PowerShell/PSScriptAnalyzer
+@{
+	IncludeRules = @(
+		# Default Rules
+		'PSUseApprovedVerbs',
+		'PSReservedCmdletChar',
+		'PSReservedParams',
+		'PSShouldProcess',
+		'PSUseShouldProcessForStateChangingFunctions',
+		'PSUseSingularNouns',
+		'PSMissingModuleManifestField',
+		'PSAvoidDefaultValueSwitchParameter',
+		'PSAvoidUsingCmdletAliases',
+		'PSAvoidUsingWMICmdlet',
+		'PSAvoidUsingEmptyCatchBlock',
+		'PSUseCmdletCorrectly',
+		'PSUseShouldProcessForStateChangingFunctions',
+		'PSAvoidUsingPositionalParameters',
+		'PSAvoidGlobalVars',
+		'PSUseDeclaredVarsMoreThanAssignments',
+		'PSAvoidUsingInvokeExpression',
+		# Formatting Rules
+		'PSPlaceOpenBrace',
+		'PSPlaceCloseBrace',
+		'PSUseConsistentWhitespace',
+		'PSUseConsistentIndentation',
+		'PSAlignAssignmentStatement'
+	)
+	Rules = @{
+		PSPlaceOpenBrace = @{
+			Enable = $true
+			OnSameLine = $true
+			NewLineAfter = $false
+			IgnoreOneLineBlock = $true
+		}
+		PSPlaceCloseBrace = @{
+			Enable = $true
+			NewLineAfter = $true
+			IgnoreOneLineBlock = $true
+			NoEmptyLineBefore = $true
+		}
+		PSUseConsistentIndentation = @{
+			Enable = $true
+			Kind = 'tab'
+			IndentationSize = 1
+		}
+		PSUseConsistentWhitespace = @{
+			Enable = $true
+			CheckOpenBrace = $true
+			CheckOpenParen = $true
+			CheckOperator = $true
+			CheckSeparator = $true
+		}
+		PSAlignAssignmentStatement = @{
+			Enable = $true
+			CheckHashtable = $false
+		}
+	}
+}
--- install.ps1
+++ install.ps1
@@ -1,73 +1,72 @@
-# If any command throws an error, stop the script immediately
-$ErrorActionPreference = "Stop"
+﻿$ErrorActionPreference = Stop
 
 # Enable TLS 1.2 since it is required for connections to GitHub
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Helper functions for pretty terminal output
-function Write-Part ($Part) { Write-Host -Object $Part -NoNewline }
-function Write-Emphasized ($Emphasis) { Write-Host -Object $Emphasis -NoNewLine -ForegroundColor "Yellow" }
-function Write-Done { Write-Host -Object "done" -NoNewline -ForegroundColor "Green"; Write-Host "." }
+function Write-Part ([string] $Part) { Write-Host -Object $Part -NoNewline }
+function Write-Emphasized ([string] $Emphasis) { Write-Host -Object $Emphasis -NoNewLine -ForegroundColor Yellow }
+function Write-Done { Write-Host -Object 'done' -NoNewline -ForegroundColor Green; Write-Host '.' }
 
 # Determine latest Deno release via GitHub API
-$LatestReleaseUri = "https://api.github.com/repos/denoland/deno/releases/latest"
-Write-Part "Downloading "; Write-Emphasized $LatestReleaseUri; Write-Part " ... "
+$LatestReleaseUri = 'https://api.github.com/repos/denoland/deno/releases/latest'
+Write-Part 'Downloading '; Write-Emphasized $LatestReleaseUri; Write-Part ' ... '
 $LatestReleaseResponse = Invoke-WebRequest -Uri $LatestReleaseUri
 Write-Done
 
-Write-Part "Determining latest Deno version: "
+Write-Part 'Determining latest Deno version: '
 $LatestRelease = ($LatestReleaseResponse | ConvertFrom-Json).tag_name
-Write-Emphasized $LatestRelease; Write-Part " ... "
+Write-Emphasized $LatestRelease; Write-Part ' ... '
 Write-Done
 
 # Create ~\.deno\bin directory if it doesn't already exist
 $DenoBin = "${Home}\.deno\bin"
 if (!(Test-Path $DenoBin)) {
-  Write-Part "Creating directory "; Write-Emphasized $DenoBin; Write-Part " ... "
-  New-Item -Path $DenoBin -ItemType "Directory" | Out-Null
-  Write-Done
+	Write-Part 'Creating directory '; Write-Emphasized $DenoBin; Write-Part ' ... '
+	New-Item -Path $DenoBin -ItemType Directory | Out-Null
+	Write-Done
 }
 
 # Download latest Deno release
 $DenoZip = "${DenoBin}\deno_win_x64.zip"
 $DownloadUri = "https://github.com/denoland/deno/releases/download/${LatestRelease}/deno_win_x64.zip"
-Write-Part "Downloading "; Write-Emphasized $DownloadUri
-Write-Part " into "; Write-Emphasized $DenoZip; Write-Part " ... "
+Write-Part 'Downloading '; Write-Emphasized $DownloadUri
+Write-Part ' into '; Write-Emphasized $DenoZip; Write-Part ' ... '
 Invoke-WebRequest -Uri $DownloadUri -OutFile $DenoZip
 Write-Done
 
 # Extract deno.exe from .zip file
-Write-Part "Extracting "; Write-Emphasized $DenoZip
-Write-Part " into "; Write-Emphasized "${DenoBin}\deno.exe"; Write-Part " ... "
+Write-Part 'Extracting '; Write-Emphasized $DenoZip
+Write-Part ' into '; Write-Emphasized "${DenoBin}\deno.exe"; Write-Part ' ... '
 Expand-Archive `
-  -Path $DenoZip `
-  -DestinationPath $DenoBin `
-  -Force # Using -Force to overwrite deno.exe if it already exists
+	-Path $DenoZip `
+	-DestinationPath $DenoBin `
+	-Force # Using -Force to overwrite deno.exe if it already exists
 Write-Done
 
 # Remove .zip file
-Write-Part "Removing "; Write-Emphasized $DenoZip; Write-Part " ... "
+Write-Part 'Removing '; Write-Emphasized $DenoZip; Write-Part ' ... '
 Remove-Item -Path $DenoZip
 Write-Done
 
 # Get Path environment variable for the current user
 $User = [EnvironmentVariableTarget]::User
-$Path = [Environment]::GetEnvironmentVariable("Path", $User)
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
 
 # Check whether Deno is in the Path already
-$Paths = $Path -split ";"
+$Paths = $Path -split ';'
 $IsInPath = `
-  $Paths -contains $DenoBin -or
-  $Paths -contains "${DenoBin}\"
+	$Paths -contains $DenoBin -or `
+	$Paths -contains "${DenoBin}\"
 
 # Add Deno to the Path if it hasn't been added already
 if (!$IsInPath) {
-  Write-Part "Adding "; Write-Emphasized $DenoBin; Write-Part " to the ";
-  Write-Emphasized "Path"; Write-Part " environment variable ... "
-  [Environment]::SetEnvironmentVariable("Path", "${Path};${DenoBin}", $User)
-  # Add Deno to the Path variable of the current terminal session
-  # so `deno` can be used immediately without restarting the terminal
-  $SessionPath = $Env:Path
-  $Env:Path = "${SessionPath};${DenoBin}"
-  Write-Done
+	Write-Part 'Adding '; Write-Emphasized $DenoBin; Write-Part ' to the '
+	Write-Emphasized 'Path'; Write-Part ' environment variable ... '
+	[Environment]::SetEnvironmentVariable('Path', "${Path};${DenoBin}", $User)
+	# Add Deno to the Path variable of the current terminal session
+	# so `deno` can be used immediately without restarting the terminal
+	$SessionPath = $Env:Path
+	$Env:Path = "${SessionPath};${DenoBin}"
+	Write-Done
 }
