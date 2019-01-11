9028f1b5c7ff8b181831fbdd34be8980fbd507e4
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-11 16:45:29 +0100

    Start making PowerShell script work cross-platform

--- install.ps1
+++ install.ps1
@@ -1,88 +1,69 @@
+#!/usr/bin/env pwsh
 # Copyright 2018 the Deno authors. All rights reserved. MIT license.
 # TODO(everyone): Keep this script simple and easily auditable.
+
 param (
-  [alias("v")]
-  [string] $version
+  [ValidatePattern('^v(\d+).(\d+).(\d+)$')]
+  [String] $Version = 'v0.2.6'
 )
 
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = 'Stop'
 
-# Enable TLS 1.2 since it is required for connections to GitHub.
+if ($PSVersionTable.PSVersion.Major -lt 6) {
+  $IsWindows = $true
+  $IsMacOS = $false
+}
+
+$DenoDir = if ($IsWindows)
+  { "${Home}\.deno\bin" } else
+  { "${Home}/.deno/bin" }
+
+$Zip = if ($IsWindows)
+  { 'zip' } else
+  { 'gz' }
+
+$DenoZip = if ($IsWindows)
+  { "${DenoDir}\deno.${Zip}" } else
+  { "${DenoDir}/deno.${Zip}" }
+
+$OS = if ($IsWindows)
+  { 'win' } else { if ($IsMacOS)
+  { 'osx' } else
+  { 'linux' } }
+
+$DenoUri = "https://github.com/denoland/deno/releases/download/${Version}/deno_${OS}_x64.${Zip}"
+
+if (!(Test-Path $DenoDir)) {
+  New-Item $DenoDir -ItemType Directory | Out-Null
+}
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest $DenoUri -Out $DenoZip
 
-# Helper functions for pretty terminal output.
-function Write-Part ([string] $Text) {
-  Write-Host $Text -NoNewline
-}
-function Write-Emphasized ([string] $Text) {
-  Write-Host $Text -NoNewLine -ForegroundColor "Yellow"
-}
-function Write-Done {
-  Write-Host " done" -NoNewline -ForegroundColor "Green";
-  Write-Host "."
-}
+if ($IsWindows) {
+  Expand-Archive $DenoZip -Destination $DenoDir -Force
+  Remove-Item $DenoZip
+} else {
+  $InputStream = New-Object IO.FileStream $DenoZip, ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
+  $OutputStream = New-Object IO.FileStream "${DenoDir}/deno", ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
+  $GzipStream = New-Object IO.Compression.GzipStream $InputStream, ([IO.Compression.CompressionMode]::Decompress)
 
-if (-not $version) {
-  # Determine latest Deno release via GitHub API.
-  $latest_release_uri = "https://api.github.com/repos/denoland/deno/releases/latest"
-  Write-Part "Downloading "; Write-Emphasized $latest_release_uri; Write-Part "..."
-  $latest_release_json = Invoke-WebRequest -Uri $latest_release_uri
-  Write-Done
+  $Buffer = New-Object Byte[](1024)
+  while ($true) {
+    $Read = $GzipStream.Read($Buffer, 0, 1024)
+    if ($Read -le 0) { break }
+    $OutputStream.Write($Buffer, 0, $Read)
+  }
 
-  Write-Part "Determining latest Deno release: "
-  $version = ($latest_release_json | ConvertFrom-Json).tag_name
-  Write-Emphasized $version; Write-Part "... "
-  Write-Done
+  $GzipStream.Close()
+  $OutputStream.Close()
+  $InputStream.Close()
+  Remove-Item $DenoZip
 }
 
-# Create ~\.deno\bin directory if it doesn't already exist
-$deno_dir = "${Home}\.deno\bin"
-if (-not (Test-Path $deno_dir)) {
-  Write-Part "Creating directory "; Write-Emphasized $deno_dir; Write-Part "..."
-  New-Item -Path $deno_dir -ItemType Directory | Out-Null
-  Write-Done
-}
+# TODO:
+# - Set PATH on Windows as in previous script
+# - Test whether setting PATH on Unix is possible via Environment API
+# - Make deno executable on Unix
 
-# Download Deno release.
-$zip_file = "${deno_dir}\deno_win_x64.zip"
-$download_uri = "https://github.com/denoland/deno/releases/download/" +
-                "${version}/deno_win_x64.zip"
-Write-Part "Downloading "; Write-Emphasized $download_uri; Write-Part "..."
-Invoke-WebRequest -Uri $download_uri -OutFile $zip_file
-Write-Done
-
-# Extract deno.exe from .zip file.
-Write-Part "Extracting "; Write-Emphasized $zip_file
-Write-Part " into "; Write-Emphasized ${deno_dir}; Write-Part "..."
-# Using -Force to overwrite deno.exe if it already exists
-Expand-Archive -Path $zip_file -DestinationPath $deno_dir -Force
-Write-Done
-
-# Remove .zip file.
-Write-Part "Removing "; Write-Emphasized $zip_file; Write-Part "..."
-Remove-Item -Path $zip_file
-Write-Done
-
-# Get Path environment variable for the current user.
-$user = [EnvironmentVariableTarget]::User
-$path = [Environment]::GetEnvironmentVariable("PATH", $user)
-
-# Check whether Deno is in the Path.
-$paths = $path -split ";"
-$is_in_path = $paths -contains $deno_dir -or $paths -contains "${deno_dir}\"
-
-# Add Deno to PATH if it hasn't been added already.
-if (-not $is_in_path) {
-  Write-Part "Adding "; Write-Emphasized $deno_dir; Write-Part " to the "
-  Write-Emphasized "PATH"; Write-Part " environment variable..."
-  [Environment]::SetEnvironmentVariable("PATH", "${path};${deno_dir}", $user)
-  # Add Deno to the PATH variable of the current terminal session
-  # so `deno` can be used immediately without restarting the terminal.
-  $env:PATH += ";${deno_dir}"
-  Write-Done
-}
-
-Write-Host ""
-Write-Host "Deno was installed successfully." -ForegroundColor "Green"
-Write-Part "Run "; Write-Emphasized "deno --help"; Write-Host " to get started."
-Write-Host ""
+Write-Host "Deno was installed successfully." -ForegroundColor Green
