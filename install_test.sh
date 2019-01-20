4fa7b085d8e5c522ff624c177df3ed8749da3f33
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-20 17:24:41 +0100

    Reset install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -2,18 +2,16 @@
 
 set -ev
 
-# Check formatting.
+# Lint.
+shellcheck -s sh ./*.sh
 shfmt -d .
 
-# Lint code.
-shellcheck -s sh ./*.sh
-
-# Test installing a specific version.
+# Test we can install a specific version.
 rm -rf ~/.deno
-sh install.sh v0.2.0
+./install.sh v0.2.0
 ~/.deno/bin/deno -v | grep -e 0.2.0
 
-# Test installing the latest version.
+# Test we can get the latest version.
 rm -rf ~/.deno
-sh install.sh
+sh ./install.sh
 ~/.deno/bin/deno -v
