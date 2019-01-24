fc955934ba7f8d05fc841a54c14672d29eed8c3c
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-24 18:57:42 +0100

    Remove end-to-end tests

--- install_test.ps1
+++ install_test.ps1
@@ -45,26 +45,3 @@ if (!($DenoVersion[0] -match 'deno: \d+\.\d+\.\d+')) {
 } else {
   Write-Output $DenoVersion
 }
-
-if ($Env:CI) {
-  if ($Env:TRAVIS) {
-    Write-Output "TRAVIS_PULL_REQUEST=$($TRAVIS_PULL_REQUEST)"
-    Write-Output "TRAVIS_PULL_REQUEST_SLUG=$($TRAVIS_PULL_REQUEST_SLUG)"
-    Write-Output "TRAVIS_PULL_REQUEST_BRANCH=$($TRAVIS_PULL_REQUEST_BRANCH)"
-    if ($Env:TRAVIS_PULL_REQUEST -ne 'false') {
-      iex (iwr "https://raw.githubusercontent.com/$($Env:TRAVIS_PULL_REQUEST_SLUG)/$($Env:TRAVIS_PULL_REQUEST_BRANCH)/install.ps1")
-    } else {
-      iex (iwr https://deno.land/x/install/install.ps1)
-    }
-  }
-  if ($Env:APPVEYOR) {
-    Write-Output "APPVEYOR_PULL_REQUEST_NUMBER=$($APPVEYOR_PULL_REQUEST_NUMBER)"
-    Write-Output "APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME=$($APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME)"
-    Write-Output "APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH=$($APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH)"
-    if (!$($Env:APPVEYOR_PULL_REQUEST_NUMBER)) {
-      iex (iwr "https://raw.githubusercontent.com/$($Env:APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME)/$($Env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH)/install.ps1")
-    } else {
-      iex (iwr https://deno.land/x/install/install.ps1)
-    }
-  }
-}
