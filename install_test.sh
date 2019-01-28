af46d42f854fffe34e2d5c449dd4457327db5754
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-28 22:33:55 +0100

    Harmonize test scripts

--- install_test.sh
+++ install_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -ev
+set -e
 
 # Lint.
 shellcheck -s sh ./*.sh
@@ -9,9 +9,13 @@ shfmt -d .
 # Test we can install a specific version.
 rm -rf ~/.deno
 ./install.sh v0.2.0
-~/.deno/bin/deno -v | grep -e 0.2.0
+version=$(~/.deno/bin/deno --version)
+echo "$version"
+test "$(echo "$version" | head -n 1)" = "deno: 0.2.0"
 
 # Test we can install the latest version.
 rm -rf ~/.deno
 sh ./install.sh
-~/.deno/bin/deno -v
+version=$(~/.deno/bin/deno --version)
+echo "$version"
+echo "$version" | head -n 1 | grep -Eq '^deno: \d+\.\d+\.\d+$'
