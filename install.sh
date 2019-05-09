a00d792daf385a1aa8450d6e88eb594f48bfb746
gamesquestionmark <37005604+gamesquestionmark@users.noreply.github.com>
2019-05-09 11:56:02 +0200

    fixed typo

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ x86_64) arch="x86_64" ;;
 esac
 
 if [ "$arch" = "other" ]; then
-	echo "Unsuported architecture $(uname -m). Only x64 binaries are available."
+	echo "Unsupported architecture $(uname -m). Only x64 binaries are available."
 	exit
 fi
 
