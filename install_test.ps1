89bac80ea3005211d73ecb65de965be7248c0a47
Ryan Dahl <ry@tinyclouds.org>
2020-03-31 11:37:41 -0400

    Use new LLVM target triple filenames
    Remove support for non-windows in powershell. I'm very sure no one uses
    it, it complicates the script, I don't care to maintain it.

--- install_test.ps1
+++ install_test.ps1
@@ -2,10 +2,6 @@
 
 $ErrorActionPreference = 'Stop'
 
-if ($PSVersionTable.PSEdition -ne 'Core') {
-  $IsWindows = $true
-}
-
 if (!(Get-PSRepository)) {
   Register-PSRepository -Default
 }
@@ -18,33 +14,18 @@ if (!(Get-Module PSScriptAnalyzer -ListAvailable)) {
 Invoke-ScriptAnalyzer *.ps1 -EnableExit -Exclude PSAvoidAssignmentToAutomaticVariable
 
 # Test that we can install the latest version at the default location.
-if ($IsWindows) {
-  Remove-Item "~\.deno" -Recurse -Force -ErrorAction SilentlyContinue
-} else {
-  Remove-Item "~/.local/bin/deno" -Force -ErrorAction SilentlyContinue
-}
+Remove-Item "~\.deno" -Recurse -Force -ErrorAction SilentlyContinue
 $env:DENO_INSTALL = ""
 .\install.ps1
-if ($IsWindows) {
-  ~\.deno\bin\deno.exe --version
-} else {
-  ~/.local/bin/deno --version
-}
+~\.deno\bin\deno.exe --version
 
 # Test that we can install a specific version at a custom location.
-if ($IsWindows) {
-  Remove-Item "~\deno-0.13.0" -Recurse -Force -ErrorAction SilentlyContinue
-  $env:DENO_INSTALL = "$Home\deno-0.13.0"
-} else {
-  Remove-Item "~/deno-0.13.0" -Recurse -Force -ErrorAction SilentlyContinue
-  $env:DENO_INSTALL = "$Home/deno-0.13.0"
-}
-.\install.ps1 v0.13.0
-$DenoVersion = if ($IsWindows) {
-  ~\deno-0.13.0\bin\deno.exe --version
-} else {
-  ~/deno-0.13.0/bin/deno --version
-}
-if (!($DenoVersion -like '*0.13.0*')) {
+Remove-Item "~\deno-0.38.0" -Recurse -Force -ErrorAction SilentlyContinue
+$env:DENO_INSTALL = "$Home\deno-0.38.0"
+
+.\install.ps1 v0.38.0
+$DenoVersion = ~\deno-0.38.0\bin\deno.exe --version
+
+if (!($DenoVersion -like '*0.38.0*')) {
   throw $DenoVersion
 }
--- install_test.sh
+++ install_test.sh
@@ -12,7 +12,7 @@ sh ./install.sh
 ~/.local/bin/deno --version
 
 # Test that we can install a specific version at a custom location.
-rm -rf ~/deno-0.13.0
-export DENO_INSTALL="$HOME/deno-0.13.0"
-./install.sh v0.13.0
-~/deno-0.13.0/bin/deno --version | grep 0.13.0
+rm -rf ~/deno-0.38.0
+export DENO_INSTALL="$HOME/deno-0.38.0"
+./install.sh v0.38.0
+~/deno-0.38.0/bin/deno --version | grep 0.38.0
