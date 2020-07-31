3c68ec65bd92c9713a86ba0a52a4acd49042de43
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-07-31 12:35:51 -0400

    ask to automatically add Deno to the PATH

--- install.sh
+++ install.sh
@@ -60,8 +60,18 @@ else
 	/bin/zsh) shell_profile=".zshrc" ;;
 	*) shell_profile=".bash_profile" ;;
 	esac
-	echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
-	echo "  export DENO_INSTALL=\"$deno_install\""
-	echo "  export PATH=\"\$DENO_INSTALL/bin:\$PATH\""
+
+	read -p "Install Deno into your PATH? [y/N]: " -r REPLY
+	if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ]; then
+		echo "" >> $HOME/$shell_profile
+		echo "# automatically added by deno_install" >> $HOME/$shell_profile
+		echo "export DENO_INSTALL=\"$deno_install\"" >> $HOME/$shell_profile
+		echo "export PATH=\"\$DENO_INSTALL/bin:\$PATH\"" >> $HOME/$shell_profile
+	else
+		echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
+		echo "  export DENO_INSTALL=\"$deno_install\""
+		echo "  export PATH=\"\$DENO_INSTALL/bin:\$PATH\""
+	fi
+
 	echo "Run '$exe --help' to get started"
 fi
