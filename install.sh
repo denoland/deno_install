fa93cd36cfb80b89e7d37eb915e092ff9cd5cce6
Maximous Black <maximousblk@gmail.com>
2020-11-10 10:45:44 +0000

    feat: use direct mirror for latest release
    use the direct mirror for downloading the latest binary, instead of using grep on the releases page

--- install.sh
+++ install.sh
@@ -5,37 +5,28 @@
 set -e
 
 if [ "$(uname -m)" != "x86_64" ]; then
-	echo "Error: Unsupported architecture $(uname -m). Only x64 binaries are available." 1>&2
-	exit 1
+  echo "Error: Unsupported architecture $(uname -m). Only x64 binaries are available." 1>&2
+  exit 1
 fi
 
 if ! command -v unzip >/dev/null; then
-	echo "Error: unzip is required to install Deno (see: https://github.com/denoland/deno_install#unzip-is-required)." 1>&2
-	exit 1
+  echo "Error: unzip is required to install Deno (see: https://github.com/denoland/deno_install#unzip-is-required)." 1>&2
+  exit 1
 fi
 
 if [ "$OS" = "Windows_NT" ]; then
-	target="x86_64-pc-windows-msvc"
+  target="x86_64-pc-windows-msvc"
 else
-	case $(uname -s) in
-	Darwin) target="x86_64-apple-darwin" ;;
-	*) target="x86_64-unknown-linux-gnu" ;;
-	esac
+  case $(uname -s) in
+  Darwin) target="x86_64-apple-darwin" ;;
+  *) target="x86_64-unknown-linux-gnu" ;;
+  esac
 fi
 
 if [ $# -eq 0 ]; then
-	deno_asset_path=$(
-		curl -sSf https://github.com/denoland/deno/releases |
-			grep -o "/denoland/deno/releases/download/.*/deno-${target}\\.zip" |
-			head -n 1
-	)
-	if [ ! "$deno_asset_path" ]; then
-		echo "Error: Unable to find latest Deno release on GitHub." 1>&2
-		exit 1
-	fi
-	deno_uri="https://github.com${deno_asset_path}"
+  deno_uri="https://github.com/denoland/deno/releases/latest/download/deno-${target}.zip"
 else
-	deno_uri="https://github.com/denoland/deno/releases/download/${1}/deno-${target}.zip"
+  deno_uri="https://github.com/denoland/deno/releases/download/${1}/deno-${target}.zip"
 fi
 
 deno_install="${DENO_INSTALL:-$HOME/.deno}"
@@ -43,7 +34,7 @@ bin_dir="$deno_install/bin"
 exe="$bin_dir/deno"
 
 if [ ! -d "$bin_dir" ]; then
-	mkdir -p "$bin_dir"
+  mkdir -p "$bin_dir"
 fi
 
 curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri"
@@ -53,14 +44,14 @@ rm "$exe.zip"
 
 echo "Deno was installed successfully to $exe"
 if command -v deno >/dev/null; then
-	echo "Run 'deno --help' to get started"
+  echo "Run 'deno --help' to get started"
 else
-	case $SHELL in
-	/bin/zsh) shell_profile=".zshrc" ;;
-	*) shell_profile=".bash_profile" ;;
-	esac
-	echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
-	echo "  export DENO_INSTALL=\"$deno_install\""
-	echo "  export PATH=\"\$DENO_INSTALL/bin:\$PATH\""
-	echo "Run '$exe --help' to get started"
+  case $SHELL in
+  /bin/zsh) shell_profile=".zshrc" ;;
+  *) shell_profile=".bash_profile" ;;
+  esac
+  echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
+  echo "  export DENO_INSTALL=\"$deno_install\""
+  echo "  export PATH=\"\$DENO_INSTALL/bin:\$PATH\""
+  echo "Run '$exe --help' to get started"
 fi
